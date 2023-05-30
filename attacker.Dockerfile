c790c95b25f5ef4eefa206e087f24009677493f9
Jose Manuel Sanchez Rico <josesanc@c2r7s1.42malaga.com>
2023-05-30 18:30:40 +0200

    Libcap removed to allow scapy and pcapy

--- attacker.Dockerfile
+++ attacker.Dockerfile
@@ -1,11 +1,18 @@
-FROM python:3.8-alpine3.17
-
+FROM python:3.8.16-bullseye
 WORKDIR /inquisitor
 
-RUN apk update && apk add tcpdump libpcap-dev
+ARG IP-src
+ARG MAC-src
+ARG IP-target
+ARG MAC-target
+
+RUN apt-get update && apt-get install python3-dev libpcap-dev libnet-dev -y
+
+RUN pip3 install --upgrade pip
 RUN pip3 install scapy
-RUN pip3 install libpcap
+RUN pip3 install argparse
+RUN pip3 install pcapy
 
 COPY inquisitor.py ./
 
-CMD [ "python3" , "inquisitor.py" ]
+CMD [ "python3" , "inquisitor.py", "$IP-src", "$MAC-src",  "$IP-target", "$MAC-target"]
--- inquisitor.py
+++ inquisitor.py
@@ -1,23 +1,43 @@
 import argparse
 import os, sys
 import signal
-import libpcap
+import argparse
 import scapy
+import threading
+# import libpcap
 
 # libcap (y libnet) o hacerlo directamente en C 
 # arp comando para ver la tabla de arp
 # devdungeon libcap
+
+
+# Every 45 seconds, refresh the ARP table
+def ft_arp_refresh(ip_src, mac_src, ip_dst, mac_dst):
+    print("Free the arp tables")
+    print(f"{ip_src} {mac_dst}")
+
+    while True: #not event.is_set():
+        pass
+    exit(0)
 
 def ft_sigint_handler(sig, frame):
     print("Free the arp tables")
     exit(0)
 
 if __name__ == "__main__":
-    assert  len(sys.argv) == 5, "Needs four arguments: <IP-src> <MAC-src> <IP-target> <MAC-target>"
+    parser = argparse.ArgumentParser(description='ARP spoofer, ./inuisitor <IP-src> <MAC-src> <IP-target> <MAC-target>')
+    parser.add_argument('addresses', action='store', nargs=4, help="<IP-src> <MAC-src> <IP-target> <MAC-target>")
+
+    args = parser.parse_args()
     signal.signal(signal.SIGINT, ft_sigint_handler)
-    libpcap.config(LIBPCAP=None)
+
+    ip_src = args.addresses[0]
+    mac_src = args.addresses[1]
+    ip_dst = args.addresses[2]
+    mac_dst = args.addresses[3]
     
-    errbuf = None
-    libpcap.lookupdev(errbuf)
+    x = threading.Thread(target=ft_arp_refresh, args=(ip_src, mac_src, ip_dst, mac_dst, ))
+    x.start()
+    x.join()
     while True:
         pass
