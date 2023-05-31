c4861ff191e1f81917ca5505be4216cf32ea9df4
Jose Manuel Sanchez Rico <josesanc@c2r8s1.42malaga.com>
2023-05-31 14:48:36 +0200

    Threads

--- inquisitor.py
+++ inquisitor.py
@@ -2,42 +2,78 @@ import argparse
 import os, sys
 import signal
 import argparse
-import scapy
+import logging
+logging.getLogger("scapy").setLevel(logging.CRITICAL)
+
+import scapy.all as scapy
+import time
 import threading
-# import libpcap
-
-# libcap (y libnet) o hacerlo directamente en C 
-# arp comando para ver la tabla de arp
-# devdungeon libcap
+import socket
 
 
-# Every 45 seconds, refresh the ARP table
-def ft_arp_refresh(ip_src, mac_src, ip_dst, mac_dst):
-    print("Free the arp tables")
-    print(f"{ip_src} {mac_dst}")
+class Spoofer(threading.Thread):
+    class SpooferRefresh(threading.Thread):
+        def __init__(self, event, ip_src, mac_src, ip_dst, mac_dst) -> None:
+            super(Spoofer.SpooferRefresh, self).__init__()
+            self.event = event
+            self.ip_src = ip_src
+            self.mac_src = mac_src
+            self.ip_dst = ip_dst
+            self.mac_dst = mac_dst
+            hostname = socket.gethostname()
+            self.ip_address = socket.gethostbyname(hostname)
 
-    while True: #not event.is_set():
-        pass
-    exit(0)
+        def run(self):
+            ''' Spoofing done every 10 seconds'''
+            seconds_to_spoof = 10
+            while not self.event.is_set():
+                print("ARP spoofing") # Spoof
+                self.event.wait(seconds_to_spoof)
+                self.ft_spoof()
+            print
+            exit(0)
 
-def ft_sigint_handler(sig, frame):
-    print("Free the arp tables")
-    exit(0)
+
+        def ft_spoof(self):
+            # scapy.send(scapy.ARP(op = 2, pdst = ip_dst, 
+            #                  hwdst = mac_src, 
+            #                            psrc = self.ip_address), verbose = False)
+            print(self.ip_address)
+            pass
+
+    def __init__(self, ip_src, mac_src, ip_dst, mac_dst) -> None:
+        super(Spoofer, self).__init__()
+        self.event = threading.Event()
+        self.spoofer_refresher = Spoofer.SpooferRefresh(self.event, ip_src, mac_src, ip_dst, mac_dst)
+        signal.signal(signal.SIGINT, self.catch)
+        signal.siginterrupt(signal.SIGINT, False)
+
+    def catch(self, signum, frame):
+        print("\r", end="")
+        print("Free the arp tables")
+        self.event.set()
+        self.spoofer_refresher.join()
+
+    def run(self):
+        self.spoofer_refresher.start()
+        while True:
+            signal.pause()
+
+
+def ft_handler(signum, frame):
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ARP spoofer, ./inuisitor <IP-src> <MAC-src> <IP-target> <MAC-target>')
     parser.add_argument('addresses', action='store', nargs=4, help="<IP-src> <MAC-src> <IP-target> <MAC-target>")
 
     args = parser.parse_args()
-    signal.signal(signal.SIGINT, ft_sigint_handler)
 
     ip_src = args.addresses[0]
     mac_src = args.addresses[1]
     ip_dst = args.addresses[2]
     mac_dst = args.addresses[3]
     
-    x = threading.Thread(target=ft_arp_refresh, args=(ip_src, mac_src, ip_dst, mac_dst, ))
-    x.start()
-    x.join()
-    while True:
-        pass
+    spoofer = Spoofer(ip_src, mac_src, ip_dst, mac_dst)
+    spoofer.start()
+    spoofer.join()
